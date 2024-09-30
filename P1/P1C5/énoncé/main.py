77ef8e8bdf455c7729bc9837e900c38abd8bc0b7
adamcoulibaly <adam.coulibaly@enkoschools.com>
2024-09-30 00:32:52 +0000

    Update main.py

--- P1/P1C5/énoncé/main.py
+++ P1/P1C5/énoncé/main.py
@@ -1,1 +1,16 @@
-# Écrivez votre code ici !
+nom = "Cheick"
+age = 19
+taille = 1.81
+est_etudiant = True
+
+# Affiché les valeurs 
+
+print (f"Nom : {nom}")
+print (f"Age : {age}")
+print (f"Taille : {taille}")
+print (f"Est etudiant : {est_etudiant}")
+
+print (f"Type de Nom : {type(nom)}")
+print (f"Type de Age : {type(age)}")
+print (f"Type de Taille : {type(taille)}")
+print (f"Type de Est etudiant : {type(est_etudiant)}")

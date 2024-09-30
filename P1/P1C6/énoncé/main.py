9cfdac85c5ea8311ffce1f46225eef2ee31ec7c2
adamcoulibaly <adam.coulibaly@enkoschools.com>
2024-09-30 00:46:57 +0000

    Update main.py

--- P1/P1C6/énoncé/main.py
+++ P1/P1C6/énoncé/main.py
@@ -1,1 +1,15 @@
-# Écrivez votre code ici !
+# Ajoutez kiwi
+fruits.append("kiwi")
+
+# Supprimer orange 
+fruits.remove("orange")
+
+# modifier le deuxieme element en ananas
+fruits[1] = "ananas"
+
+# donner la longueur de la liste
+print ("la liste de fruits ce compose de", len(fruits), "elements")
+
+# triez par ordre alphabetique
+fruits.sort()
+print(fruits)

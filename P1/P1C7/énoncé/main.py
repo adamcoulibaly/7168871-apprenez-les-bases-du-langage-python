fb8376a5d0c0fe0b138ec2ad5aada55abb1e909f
adamcoulibaly <adam.coulibaly@enkoschools.com>
2024-10-01 16:30:56 +0000

    Update main.py

--- P1/P1C7/énoncé/main.py
+++ P1/P1C7/énoncé/main.py
@@ -1,1 +1,23 @@
-# Écrivez votre code ici !
+# practice 
+
+# dictionnaire
+
+fruits = {"pomme":"rouge", "banane":"jaune", "orange":"orange"}
+
+# ajout de la cle 
+
+fruits["kiwi"] = "vert" 
+
+#acces a la cle banane
+
+couleur_banane = fruits["banane"]
+
+# modifcation de la valeur associe a la cle
+
+fruits["pomme"] = "vert"
+
+# suppression de la cle 
+del fruits["banane"]
+
+# affiche la cle 
+print(fruits.keys())

ccb9d9d9cf6d84f14dbcd70406ddbbf59ec33355
cheick adam Coulibaly <adam.coulibaly@enkoschools.com>
2026-03-08 21:32:21 +0000

    Coulibaly Cheick Adam (exo)

--- P1/P1C7/énoncé/main.py
+++ P1/P1C7/énoncé/main.py
@@ -1,1 +1,22 @@
-# Écrivez votre code ici !
+# dictionnaire de fruits
+fruits = {
+    "pomme" : "rouge",
+    "banane" : "jaune",
+    "orange" : "orange"
+}
+
+# ajout de kiwi au dictionnaire
+fruits['kiwi'] = "verts"
+print(fruits)
+
+# modifier la couleur de la pomme
+fruits['pomme'] = "vert"
+print(fruits)
+
+# supprimer la banane du dictionnaire
+del fruits['banane']
+print(fruits)
+
+# afficher les valeurs du dictionnaire
+print(fruits.keys())
+print(fruits.values())

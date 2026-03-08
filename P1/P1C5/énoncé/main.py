34cb887b116eda91e9f6e6214e26c083c854c9cf
cheick adam Coulibaly <adam.coulibaly@enkoschools.com>
2026-03-08 13:47:56 +0000

    Coulibaly Cheick Adam (exo)

--- P1/P1C5/énoncé/main.py
+++ P1/P1C5/énoncé/main.py
@@ -1,1 +1,7 @@
-# Écrivez votre code ici !
+nom = "Coulibaly Cheick Adam"
+age = 21
+taille = 1.80
+est_etudiant = True
+
+print(type(nom), type(age), type(taille), type(est_etudiant))
+print(f"Bonjour, je m'appelle {nom}, j'ai {age} ans et je mesure {taille} metre. Est-ce que je suis etudiant? {est_etudiant}")

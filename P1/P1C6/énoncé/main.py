43d5fd2cf3849632a63d7d1b625ef8a3dca5a610
cheick adam Coulibaly <adam.coulibaly@enkoschools.com>
2026-03-08 14:37:42 +0000

    Coulibaly Cheick Adam (exo)

--- P1/P1C6/énoncé/main.py
+++ P1/P1C6/énoncé/main.py
@@ -1,1 +1,18 @@
-# Écrivez votre code ici !
+#liste de fruits
+fruits = ["pomme", "banane", "orange"]
+
+#afficher la liste
+print(fruits)
+
+# ajouter un fruit a la liste
+fruits.append("kiwi")
+print(fruits)
+
+# retirer un fruit a la liste
+fruits.remove("orange")
+fruits.insert(1, "ananas")
+
+#afficher la liste
+print(fruits)
+print(len(fruits))
+print(sorted(fruits))
